7bfca47bac78c2ec78c93ae1feadff28a702407e
phurhard <phurhardeen@gmail.com>
2023-12-22 16:29:58 +0100

    no reasonable message here

--- Student/urls.py
+++ Student/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from wkhtmltopdf.views import PDFTemplateView
+# from wkhtmltopdf.views import PDFTemplateView
 
 urlpatterns = [
     path('student/', views.student_profile, name='home_student'),
     path('students/', views.students_list, name='student_list'),
+    path('result/print/', views.resultToPDF, name='pdf'),
     path('student/<int:id>/result', views.results, name='students_result'),
-    path('student/pdf/', PDFTemplateView.as_view(template_name='Students/results.html', filename='result.pdf'), name='pdfResult'),
+    # path('student/pdf/', PDFTemplateView.as_view(template_name='Students/results.html', filename='result.pdf'), name='pdfResult'),
 ]
--- Student/views.py
+++ Student/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 from main.models import Student
+from django.template import engines
 
 # Create your views here.
 
@@ -21,3 +22,43 @@ def results(request, id) -> HttpResponse:
     '''Returns the result of the specific student'''
     student: Student = get_object_or_404(Student, id=id)
     return render(request, 'Students/results.html', {'student': student})
+
+def resultToPDF(request):
+    '''returns a pdf of teacher subjects'''
+    # Render the Jinja template
+    template = engines['django'].from_string(open('Student/templates/Students/result.html').read())
+    html_content = template.render({'student': request.user})
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="report.pdf"'
+
+    # Use ReportLab to convert HTML to PDF
+    from reportlab.pdfgen import canvas
+    from reportlab.lib.pagesizes import letter
+    # from django.http import HttpResponse
+
+    pdf_file = response
+
+    p = canvas.Canvas(pdf_file, pagesize=letter)
+    width, height = letter
+
+    # from reportlab.lib.pagesizes import letter
+    from reportlab.lib import colors
+    from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle
+    from reportlab.lib.styles import getSampleStyleSheet
+
+    # pdf_file = response
+    doc = SimpleDocTemplate(pdf_file)
+    story = []
+
+    styles = getSampleStyleSheet()
+
+    # Add the HTML content to the PDF
+    paragraphs = []
+    paragraphs.extend(Paragraph(line, styles["BodyText"]) for line in html_content.splitlines())
+
+    story.extend(paragraphs)
+
+    doc.build(story)
+
+    return response
